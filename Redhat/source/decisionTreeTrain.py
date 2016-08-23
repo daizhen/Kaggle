cfab1587c009180719842dc823aa12e44dafe872
Dai Zhen <daizhenbest@gmail.com>
2016-08-23 23:29:08 +0800

    Update code

--- Redhat/source/decisionTreeTrain.py
+++ Redhat/source/decisionTreeTrain.py
@@ -3,8 +3,14 @@ import pandas as pd
 def Train():
     clf = tree.DecisionTreeClassifier()
     data = pd.read_csv('../data/merged_train.csv')
-    selectedColumns = data.columns!=u'activity_id'
-    print list(data.columns)
-    newFrame = data[ list(data.columns)]
+
+    columnsList = list(data.columns.values)
+    selectedColumnList = [columnName for columnName in columnsList  if columnName!=u'activity_id' and columnName!=u'outcome']
+
+    print list(selectedColumnList)
+    print len(selectedColumnList)
+    newFrame = data[ selectedColumnList]
     print newFrame.tail()
+
+    clf.fit(X=newFrame.values, y=data['outcome'].values);
 Train()
