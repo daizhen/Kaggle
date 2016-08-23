ab0a32a89f598dc45c9c657ff1e4d2831633bbd0
Dai Zhen <daizhenbest@gmail.com>
2016-08-24 07:21:42 +0800

    split data into train, validatoin, test sets

--- Redhat/source/decisionTreeTrain.py
+++ Redhat/source/decisionTreeTrain.py
@@ -1,16 +1,20 @@
-from  sklearn import tree
+from sklearn import tree
 import pandas as pd
 def Train():
     clf = tree.DecisionTreeClassifier()
-    data = pd.read_csv('../data/merged_train.csv')
+    data = pd.read_csv('../data/testing_data.csv')
 
     columnsList = list(data.columns.values)
-    selectedColumnList = [columnName for columnName in columnsList  if columnName!=u'activity_id' and columnName!=u'outcome']
+    selectedColumnList = [columnName for columnName in columnsList  if columnName!=u'activity_id' and columnName!=u'outcome' and columnName!=u'people_id']
 
     print list(selectedColumnList)
     print len(selectedColumnList)
     newFrame = data[ selectedColumnList]
     print newFrame.tail()
+    print newFrame.columns
+    #clf.fit(X=newFrame.values, y=data['outcome'].values);
 
-    clf.fit(X=newFrame.values, y=data['outcome'].values);
+def PreProcessData():
+    #
+    pass
 Train()
